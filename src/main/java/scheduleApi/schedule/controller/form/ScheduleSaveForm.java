2af9bdd818773db24512146e5c3e42f12b14e05e
pjhneverdie <pjhneverdie@gmail.com>
2024-06-04 05:25:52 +0900

    form에 세터 제거해서 역할을 명확히 함

--- src/main/java/scheduleApi/schedule/controller/form/ScheduleSaveForm.java
+++ src/main/java/scheduleApi/schedule/controller/form/ScheduleSaveForm.java
@@ -5,19 +5,23 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 public class ScheduleSaveForm {
     @NotNull
-    private LocalDateTime dateTime;
+    private final LocalDateTime dateTime;
 
     @Size(min = 1, max = 25, message = "제목은 1자 이상 25자 이하여야 합니다.")
     @NotBlank(message = "제목을 입력해 주세요.")
-    private String title;
+    private final String title;
 
-    private String memo;
+    private final String memo;
+
+    public ScheduleSaveForm(LocalDateTime dateTime, String title, String memo) {
+        this.dateTime = dateTime;
+        this.title = title;
+        this.memo = memo;
+    }
 }
--- src/main/java/scheduleApi/schedule/controller/form/ScheduleUpdateForm.java
+++ src/main/java/scheduleApi/schedule/controller/form/ScheduleUpdateForm.java
@@ -5,22 +5,27 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 public class ScheduleUpdateForm {
     @NotNull
-    private int id;
+    private final int id;
 
     @NotNull
-    private LocalDateTime dateTime;
+    private final LocalDateTime dateTime;
 
     @Size(min = 1, max = 25, message = "제목은 1자 이상 25자 이하여야 합니다.")
     @NotBlank(message = "제목을 입력해 주세요.")
-    private String title;
+    private final String title;
 
-    private String memo;
+    private final String memo;
+
+    public ScheduleUpdateForm(int id, LocalDateTime dateTime, String title, String memo) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.title = title;
+        this.memo = memo;
+    }
 }
